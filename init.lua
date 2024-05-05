05b7a2ff9d8f6a61fc78f98ae72970c5125a9585
Vishnu <5123344+vishnu350@users.noreply.github.com>
2024-05-05 19:53:32 +0800

    Fixed branch versioning.

--- init.lua
+++ init.lua
@@ -234,9 +234,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  local lazyversion = 'bef521a' -- Stable version
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch', lazyversion, lazyrepo, lazypath }
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
