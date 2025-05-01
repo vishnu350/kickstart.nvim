a2355e5ddbf1c1bf6a948b915e94af85f3a8f5a3
Vishnu <5123344+vishnu350@users.noreply.github.com>
2025-05-01 14:43:46 +0800

    Updated image.nvim settings.

--- init.lua
+++ init.lua
@@ -903,9 +903,10 @@ require('lazy').setup({
       integrations = {
         markdown = {
           enabled = true,
-          clear_in_insert_mode = false,
+          clear_in_insert_mode = true,
           download_remote_images = false,
           only_render_image_at_cursor = false,
+          only_render_image_at_cursor_mode = 'popup',
           floating_windows = false, -- if true, images will be rendered in floating markdown windows
           filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
         },
