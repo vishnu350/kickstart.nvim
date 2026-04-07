5c62027f9883cef826bb6dfb141da2e98d74245a
Vishnu <5123344+vishnu350@users.noreply.github.com>
2026-04-07 17:12:15 +0800

    Reduced image sizes.

--- init.lua
+++ init.lua
@@ -734,7 +734,7 @@ require('lazy').setup({
       max_width = nil,
       max_height = nil,
       max_width_window_percentage = nil,
-      max_height_window_percentage = 33,
+      max_height_window_percentage = 20,
       window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
       window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
       editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
